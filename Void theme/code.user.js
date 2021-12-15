72eb640d2d7795e6b01404175508aa190707b168
TouchedByDarkness <53915513+TouchedByDarkness@users.noreply.github.com>
2021-12-15 16:12:46 +0500

    Create code.user.js

---
+++ Void theme/code.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Void theme
+// @version      1.0
+// @description  Void theme for pxls.space
+// @author       Darkness
+// @match        https://pxls.space/*
+// @icon         https://raw.githubusercontent.com/TouchedByDarkness/pxls-space/main/icon.png
+// ==/UserScript==
+
+const source = 'https://raw.githubusercontent.com/TouchedByDarkness/pxls-space/main/Void theme/code.js';
+
+fetch(source)
+.then(res => res.text())
+.then(code => {
+  const el = document.createElement('script');
+  el.innerHTML = code;
+  document.body.appendChild(el);
+})
